9cd6d2779bd9c20b9d1dac290eea7819e0c2f272
Mahesh Reddy <mahesh@singular.dev>
2023-02-01 21:39:19 +0530

    Changed default credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'sample@example.com', password: 'galaxy@2428', password_confirmation: 'galaxy@2428') if Rails.env.development?
