8d86c69db24aeefce098ec32b4c71d42e1c5bfde
Mahesh Reddy <mahesh@singular.dev>
2023-02-03 00:13:22 +0530

    Added Roster Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resource :rosters
+
   #root_to = 'admin/dashboard#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ test/decorators/roster_decorator_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class RosterDecoratorTest < Draper::TestCase
+end
